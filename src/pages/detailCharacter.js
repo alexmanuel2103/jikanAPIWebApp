1eec2ab51358245fa658f75478a36e81b26d66c4
Manuel Barraza <barrazaalexmanuel@gmail.com>
2022-03-07 08:35:02 -0700

    Fixing design errors

--- src/pages/detailCharacter.js
+++ src/pages/detailCharacter.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import http from "../lib/request";
-import "../pages/styles.css"
+import "../styles.css"
 
 //Jonatan's part 
  const DetailCharacter = () =>{
